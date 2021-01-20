a3cb89ce23e67d3638c9631cb91192bb6599e252
kghite <kghite@gmail.com>
2021-01-20 15:47:53 -0500

    ADD: Open existing files

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,12 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
 	pub x: usize,
 	pub y: usize,
@@ -12,6 +16,7 @@ pub struct Editor {
 	should_quit: bool,
 	terminal: Terminal,
 	cursor_position: Position,
+	document: Document,
 }
 
 impl Editor {
@@ -30,10 +35,20 @@ impl Editor {
 	}
 
 	pub fn default() -> Self {
+		// Get input filename
+		let args: Vec<String> = env::args().collect();
+		let document = if args.len() > 1 {
+			let filename = &args[1];
+			Document::open(&filename).unwrap_or_default()
+		} else {
+			Document::default()
+		}; // ; here and not inside ensures doc is never undefined
+
 		Self {
 			should_quit: false,
 			terminal: Terminal::default().expect("Failed to init terminal"), 
-			cursor_position: Position { x: 0, y: 0 },
+			cursor_position: Position::default(),
+			document,
 		}
 	}
 
@@ -42,7 +57,7 @@ impl Editor {
 	*/
 	fn refresh_screen(&self) -> Result<(), std::io::Error> {
 		Terminal::cursor_hide();
-		Terminal::cursor_position(&Position { x: 0, y: 0 });
+		Terminal::cursor_position(&Position::default());
 
 		if self.should_quit {
 			println!("Goodbye.\r");
@@ -123,15 +138,27 @@ impl Editor {
 		msg.truncate(width);
 		println!("{}\r", msg);
 	}
+
+	/* 
+		Draw document rows
+	*/
+	pub fn draw_row(&self, row: &Row) {
+		let start = 0;
+		let end = self.terminal.size().width as usize;
+		let row = row.render(start, end);
+		println!("{}\r", row)
+	}
 	
 	/*
-		Draw margin features
+		Draw terminal row features
 	*/
 	fn draw_rows(&self) {
 		let height = self.terminal.size().height;
-		for row in 0..height - 1 {	
+		for terminal_row in 0..height - 1 {	
 			Terminal::clear_current_line();
-			if row == height / 3 {
+			if let Some(row) = self.document.row(terminal_row as usize) {
+				self.draw_row(row);
+			} else if self.document.is_empty() && terminal_row == height / 3 {
 				self.draw_welcome_msg();
 			} else {
 				println!("~\r");
